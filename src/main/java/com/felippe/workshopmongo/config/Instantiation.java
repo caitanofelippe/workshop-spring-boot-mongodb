bfb8acd2ce88ee99215d978dc68c7f099b7c693e
Felippe Caitano <caitano.felippe@gmail.com>
2020-08-14 15:59:18 -0300

    Operacao de instanciacao da base de dados

---
+++ src/main/java/com/felippe/workshopmongo/config/Instantiation.java
@@ -0,0 +1,37 @@
+package com.felippe.workshopmongo.config;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.omg.CORBA.UserException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import com.felippe.workshopmongo.domain.User;
+import com.felippe.workshopmongo.repository.UserRepository;
+
+@Configuration
+public class Instantiation implements CommandLineRunner {
+
+	@Autowired
+	private UserRepository userRepository;
+
+	@Override
+	public void run(String... args) throws Exception {
+
+		userRepository.deleteAll();
+
+		User maria = new User(null, "Maria Brown", "maria@gmail.com");
+		User alex = new User(null, "Alex Green", "alex@gmail.com");
+		User bob = new User(null, "Bob Grey", "bob@gmail.com");
+		
+		List<User> list = new ArrayList<User>();
+		list.addAll(Arrays.asList(maria, alex, bob));
+
+		userRepository.saveAll(list);
+
+	}
+
+}
