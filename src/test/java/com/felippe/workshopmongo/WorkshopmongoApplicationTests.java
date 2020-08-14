ff34307d719f07e2642efbdc477428e53fe40dea
Felippe Caitano <caitano.felippe@gmail.com>
2020-08-14 14:20:02 -0300

    Primeiro commit do projeto criado

---
+++ src/test/java/com/felippe/workshopmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.felippe.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
