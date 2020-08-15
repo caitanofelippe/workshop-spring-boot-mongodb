fa8141aa7c8db6c1861e60877f13647f0841ab1f
Felippe Caitano <caitano.felippe@gmail.com>
2020-08-15 13:59:25 -0300

    Obtendo post por id

---
+++ src/main/java/com/felippe/workshopmongo/services/PostService.java
@@ -0,0 +1,26 @@
+package com.felippe.workshopmongo.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.felippe.workshopmongo.domain.Post;
+import com.felippe.workshopmongo.domain.User;
+import com.felippe.workshopmongo.dto.UserDTO;
+import com.felippe.workshopmongo.repository.PostRepository;
+import com.felippe.workshopmongo.repository.UserRepository;
+import com.felippe.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Post não encontrado"));
+	}
+}
