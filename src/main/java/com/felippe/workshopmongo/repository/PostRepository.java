06861c83fb12b81536cb1d0198e9b62567a76ef5
Felippe Caitano <caitano.felippe@gmail.com>
2020-08-15 14:44:04 -0300

    Consulta simples com query methods

--- src/main/java/com/felippe/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/felippe/workshopmongo/repository/PostRepository.java
@@ -1,5 +1,7 @@
 package com.felippe.workshopmongo.repository;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,6 +11,8 @@ import com.felippe.workshopmongo.domain.User;
 @Repository
 public interface PostRepository extends MongoRepository<Post, String>{
 	
+	
+	List<Post> findByTitleContainingIgnoreCase(String text);
 	
 
 }
